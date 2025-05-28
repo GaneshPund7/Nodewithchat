44bb0cf74389c666214383f05b22640583577a9d
Ganesh-Nimap <ganesh.pund@nimapinfotech.in>
2025-05-28 19:17:01 +0530

    changes on date 28/05/2025

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const { createServer } = require('node:http');
-const { join } = require('node:path');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',  
+    methods: ['GET', 'POST']
+  }
+});
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
+app.use(cors());
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 
- 
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
     io.emit('chat message', msg);  
   });
 
-  
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
-// Start server
 server.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
